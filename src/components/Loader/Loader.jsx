052ce5b3cf8616d3310209c467a1b78007bc6f18
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-07-29 22:11:26 +0200

    +

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { BallTriangle } from 'react-loader-spinner'
 
 const Loader = () => {
+
     return (
-        <div>Loading...</div>
+        <BallTriangle
+            height={100}
+            width={100}
+            radius={5}
+            color="rgba(183, 42, 42, 0.951)"
+            ariaLabel="ball-triangle-loading"
+            wrapperClass={{}}
+            wrapperStyle=''
+            visible={true}
+        />
     )
 }
 
