a913f36cde763f751a651011f3fef8dec8d8a3e9
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-07-29 11:45:48 +0000

    Deploying to gh-pages from @ InnaAnatoliivna/goit-react-hw-05-movies@ffb673ed55c3ec7d9bdf33929eb6d1207226c511 🚀

---
+++ static/js/247.094a5251.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[247],{247:function(e,t,r){r.r(t);r(791);var n=r(689),o=r(184);t.default=function(){var e=(0,n.s0)();return setTimeout((function(){return e("/")}),3e3),(0,o.jsxs)("h2",{children:["404 Not Found. ",(0,o.jsx)("br",{})," Will be redicted to the home page!"]})}}}]);
+//# sourceMappingURL=247.094a5251.chunk.js.map
